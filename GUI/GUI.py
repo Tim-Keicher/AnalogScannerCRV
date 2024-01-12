a028dd06c8852f6e65cdee5469b1216dfcdc0932
Tim-Keicher <keicher04@gmail.com>
2024-01-12 17:35:49 +0100

    comment code

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -38,83 +38,96 @@ class App(ctk.CTk):
     Note:
         This class inherits from the ctk.CTk class provided by the customtkinter library.
     """
-    # __init__ function for class tkinterApp
     def __init__(self, *args, **kwargs):
         # Set variables
-        self.load_location_path:str = None
-        self.save_location_path:str = None
-        self.current_camera_port:str = None
+        self.load_location_path: str = None
+        self.save_location_path: str = None
+        self.current_camera_port: str = None
 
-        # __init__ function for class Tk
+        # Initialize Tkinter
         ctk.CTk.__init__(self, *args, **kwargs)
-        
-        # Set the width and height for the window
+
+        # Set window dimensions
         self.geometry(f"{1100}x{580}")
-        #self.after(0, lambda:self.state('zoomed'))
 
         # Set window title
         self.title('Analog Scanner')
 
-        # Set window bitmap icon
-        self.after(201, lambda:self.iconbitmap('imagesGUI\HHN_Logo_D_weiss.ico'))
+        # Set window icon
+        self.after(201, lambda: self.iconbitmap('imagesGUI\HHN_Logo_D_weiss.ico'))
 
-        # Set GUI image path
+        # Set GUI image paths
         gui_image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "imagesGUI")
         self.logo_image = ctk.CTkImage(Image.open(os.path.join(gui_image_path, "HHN_Logo_D_weiss.png")), size=(138, 58))
         self.play_icon = ctk.CTkImage(Image.open(os.path.join(gui_image_path, "play_white_icon.jpg")), size=(20, 20))
         self.save_icon = ctk.CTkImage(Image.open(os.path.join(gui_image_path, "save_white_icon.jpg")), size=(20, 20))
         self.load_icon = ctk.CTkImage(Image.open(os.path.join(gui_image_path, "folder_white_icon.jpg")), size=(20, 20))
-        
-        #----------------------------------------------------------------------------------------------------
-        # configure grid layout (4x4)
+
+        # Configure grid layout (4x4)
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        # create sidebar frame with widgets
+        # Create sidebar frame with widgets
         self.sidebar_frame = ctk.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=7, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(7, weight=1)
 
+        # Label with HHN logo
         self.navigation_frame_label = ctk.CTkLabel(self.sidebar_frame, text=None, image=self.logo_image,
-                                                             compound="center", corner_radius=8) #fg_color="#b5b5b5", corner_radius=8)
+                                                   compound="center", corner_radius=8)
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
 
-        self.sidebar_camera_image = ctk.CTkOptionMenu(self.sidebar_frame, values=["Image", "Camera"], command=self.sidebar_cam_img_event)
+        # OptionMenu for choosing between Image and Camera
+        self.sidebar_camera_image = ctk.CTkOptionMenu(self.sidebar_frame, values=["Image", "Camera"],
+                                                      command=self.sidebar_cam_img_event)
         self.sidebar_camera_image.grid(row=1, column=0, padx=20, pady=10)
 
-        self.sidebar_load = ctk.CTkButton(self.sidebar_frame, text="Load Location", image=self.load_icon, anchor='w', command=self.sidebar_btn_load_event)    # need load button as soon as image is selected
+        # Button to load image location
+        self.sidebar_load = ctk.CTkButton(self.sidebar_frame, text="Load Location", image=self.load_icon, anchor='w',
+                                          command=self.sidebar_btn_load_event)
         self.sidebar_load.grid(row=2, column=0, padx=20, pady=10)
-        self.sidebar_camera_port = ctk.CTkOptionMenu(self.sidebar_frame, values=self.get_connected_camera_ports(), command=self.sidebar_cam_port_event) # need port dropdown as soon as camera is selected
-        #self.sidebar_camera_port.grid(row=2, column=0, padx=20, pady=10)   # show sidebar_load button first
 
-        self.sidebar_img_format = ctk.CTkOptionMenu(self.sidebar_frame, values=["Small Format", "Middle Format"], command=self.sidebar_format_event)
+        # OptionMenu for selecting the camera port
+        self.sidebar_camera_port = ctk.CTkOptionMenu(self.sidebar_frame, values=self.get_connected_camera_ports(),
+                                                      command=self.sidebar_cam_port_event)
+        # self.sidebar_camera_port.grid(row=2, column=0, padx=20, pady=10)  # Show sidebar_load button first
+
+        # OptionMenu for choosing image format
+        self.sidebar_img_format = ctk.CTkOptionMenu(self.sidebar_frame, values=["Small Format", "Middle Format"],
+                                                    command=self.sidebar_format_event)
         self.sidebar_img_format.grid(row=3, column=0, padx=20, pady=10)
 
+        # Checkbox to enable/disable image saving
         self.sidebar_save_cb = ctk.CTkCheckBox(self.sidebar_frame, text="Save Images", command=self.sidebar_cb_save_event)
         self.sidebar_save_cb.grid(row=4, column=0, padx=20, pady=10)
 
-        self.sidebar_save_location = ctk.CTkButton(self.sidebar_frame, text="Save Location", image=self.save_icon, anchor='w', command=self.sidebar_btn_save_event)
+        # Button to set save location
+        self.sidebar_save_location = ctk.CTkButton(self.sidebar_frame, text="Save Location", image=self.save_icon,
+                                                    anchor='w', command=self.sidebar_btn_save_event)
         self.sidebar_save_location.grid(row=5, column=0, padx=20, pady=10)
 
-        self.sidebar_process = ctk.CTkButton(self.sidebar_frame, text="Process", image=self.play_icon, anchor='w', command=self.sidebar_btn_process_event)
+        # Button to initiate image processing
+        self.sidebar_process = ctk.CTkButton(self.sidebar_frame, text="Process", image=self.play_icon, anchor='w',
+                                             command=self.sidebar_btn_process_event)
         self.sidebar_process.grid(row=6, column=0, padx=20, pady=10)
 
+        # Label and OptionMenu for selecting appearance mode
         self.appearance_mode_label = ctk.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=7, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = ctk.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
-                                                                       command=self.change_appearance_mode_event)
+                                                             command=self.change_appearance_mode_event)
         self.appearance_mode_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 10))
 
+        # Label and OptionMenu for selecting UI scaling
         self.scaling_label = ctk.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
         self.scaling_label.grid(row=9, column=0, padx=20, pady=(10, 0))
         self.scaling_optionemenu = ctk.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
-                                                               command=self.change_scaling_event)
+                                                     command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=10, column=0, padx=20, pady=(10, 20))
 
         # Set default values
         self.sidebar_camera_image.set("Image")
-        #self.sidebar_camera_port.set("port-1")
         self.sidebar_img_format.set("Small Format")
         self.sidebar_save_cb.deselect()
         self.sidebar_save_location.configure(state="disabled")
